3717ddaad3802df1e5acfaa29b8644088d9f8325
Immortalem <Immo2208@live.de>
2019-11-08 14:39:56 +0100

    Update JsseTlsServer.java

--- src/main/java/de/rub/nds/jsse/JsseTlsServer.java
+++ src/main/java/de/rub/nds/jsse/JsseTlsServer.java
@@ -24,7 +24,9 @@ import javax.net.ssl.TrustManagerFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
+import javax.net.ssl.X509ExtendedTrustManager;
+import java.security.cert.X509Certificate; 
+    
 public class JsseTlsServer {
 
     private static final Logger LOGGER = LogManager.getLogger(JsseTlsServer.class);
