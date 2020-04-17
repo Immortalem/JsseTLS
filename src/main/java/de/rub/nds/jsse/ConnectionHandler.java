92ffe248b050f2ab02f992aee9296378097fa9f9
Mario Korth <mario.korth@rub.de>
2020-04-17 14:55:28 +0200

    ..

--- src/main/java/de/rub/nds/jsse/ConnectionHandler.java
+++ src/main/java/de/rub/nds/jsse/ConnectionHandler.java
@@ -1,10 +1,13 @@
 package de.rub.nds.jsse;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.util.Strings;
 
 public class ConnectionHandler implements Runnable {
 
@@ -26,11 +29,24 @@ public class ConnectionHandler implements Runnable {
     public void run() {
 
         LOGGER.debug("new Thread started");
+
+        try {
+            final BufferedReader br = new BufferedReader(new InputStreamReader(applicationSocket.getInputStream()));
+            final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(applicationSocket.getOutputStream()));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                LOGGER.debug(line);
+                bw.write("ack");
+                bw.flush();
+            }
+        } catch (IOException e) {
+            LOGGER.debug(e.getLocalizedMessage(), e);
+        } finally {
             try {
                 applicationSocket.close();
             } catch (final IOException ioe) {
                 LOGGER.debug(ioe.getLocalizedMessage(), ioe);
             }
-        
+        }
     }
 }
